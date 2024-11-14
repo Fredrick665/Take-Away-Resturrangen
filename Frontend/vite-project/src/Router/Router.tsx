832ad357d4f928b44448887b94fa6e758f10f788
Fredrick665 <fredrickaldrin@gmail.com>
2024-11-14 20:25:01 +0100

    Fixat huvuddelen av cssn på pages.

--- Frontend/vite-project/src/Router/Router.tsx
+++ Frontend/vite-project/src/Router/Router.tsx
@@ -8,7 +8,9 @@ import EditItemsPage from "../pages/EditItemsPage/EditItemsPage";
 import OrderHistoryPage from "../pages/OrderHistoryPage/OrderHistoryPage";
 import OrderPage from "../pages/OrderPage/OrderPage";
 import ProfilePage from "../pages/ProfilePage/ProfilePage";
+import Adminpage from "../pages/AdminPage/Adminpage";
 import SingleOrderPage from "../pages/SingleOrderPage/SingleOrderPage";
+import UpdatePage from "../pages/UpdatePage/UpdatePage";
 
 function Router() {
   return (
@@ -21,8 +23,10 @@ function Router() {
       <Link to="/order-history">Order History</Link>
       <Link to="/order">Order</Link>
       <Link to="/profile">Profile</Link>
+      <Link to="/Admin">Admin</Link>
       <Link to="/single-order">Single Order</Link>
       <Link to="/about">About</Link>
+      <Link to="/update">UpdatePage</Link>
       <Routes>
         <Route path="/add-item" element={<AddItemPage />} />
         <Route path="/all-orders" element={<AllOrdersPage />} />
@@ -32,8 +36,10 @@ function Router() {
         <Route path="/order-history" element={<OrderHistoryPage />} />
         <Route path="/order" element={<OrderPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/Admin" element={<Adminpage />} />
         <Route path="/single-order" element={<SingleOrderPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/update" element={<UpdatePage />} />
       </Routes>
     </BrowserRouter>
   );
--- Frontend/vite-project/src/pages/OrderPage/OrderPage.tsx
+++ Frontend/vite-project/src/pages/OrderPage/OrderPage.tsx
@@ -1,4 +1,38 @@
+import "./orderPage.css";
+
 function OrderPage() {
-  return <p>OrderPage</p>;
+  return (
+    <main className="order-page">
+      <span className="order-page__hbmenu"></span>
+      <h1 className="order-page__title">Lagerstatus på ingredienser</h1>
+      <ul className="order-page__list">
+        <li className="order-page__list-item">
+          <label className="order-page__list-item-label">
+            Lorem ipsum dolor sit amet
+          </label>
+          <input type="number" className="order-page__number" id="item1" />
+        </li>
+        <li className="order-page__list-item">
+          <label className="order-page__list-item-label">
+            Integer at ornare nibh
+          </label>
+          <input type="number" className="order-page__number" id="item2" />
+        </li>
+        <li className="order-page__list-item">
+          <label className="order-page__list-item-label">
+            Suspendisse venenatis elit
+          </label>
+          <input type="number" className="order-page__number" id="item3" />
+        </li>
+        <li className="order-page__list-item">
+          <label className="order-page__list-item-label">
+            Maecenas id dui egestas
+          </label>
+          <input type="number" className="order-page__number" id="item4" />
+        </li>
+      </ul>
+    </main>
+  );
 }
+
 export default OrderPage;
