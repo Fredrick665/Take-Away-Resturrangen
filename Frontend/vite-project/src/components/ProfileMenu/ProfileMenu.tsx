cba89380ddc74471e4b814d9e03053c652d86156
Fredrick665 <fredrickaldrin@gmail.com>
2024-11-18 02:14:39 +0100

    Fixat cssn på på Update och Profile Page.

--- Frontend/vite-project/src/components/ProfileMenu/ProfileMenu.tsx
+++ Frontend/vite-project/src/components/ProfileMenu/ProfileMenu.tsx
@@ -1,15 +1,16 @@
+import "./profileMenu.css";
 import { Link } from "react-router-dom";
 function ProfileMenu() {
   return (
     <>
       <Link to="/orderhistory">
-        <h2 className="profile-page__heading">Orderhistorik</h2>
+        <h2 className="profile-menu__heading">Orderhistorik</h2>
       </Link>
       <Link to="/confirmedorders">
-        <h2 className="profile-page__heading">Orderstatus</h2>
+        <h2 className="profile-menu__heading">Orderstatus</h2>
       </Link>
       <Link to="/homepage">
-        <h2 className="profile-page__heading">Huvudsidan</h2>
+        <h2 className="profile-menu__heading">Huvudsidan</h2>
       </Link>
     </>
   );
