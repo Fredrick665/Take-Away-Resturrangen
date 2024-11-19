9d375f8d7aff16da28b2f4f7aabef73c55af00e2
Fredrick665 <fredrickaldrin@gmail.com>
2024-11-19 10:53:31 +0100

    Lagt till routing överallt så man kan beställa. Plus fixat add item page

--- Frontend/vite-project/src/components/Footer/Footer.tsx
+++ Frontend/vite-project/src/components/Footer/Footer.tsx
@@ -1,22 +1,37 @@
-import './footer.css'
+import "./footer.css";
+import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="footer">
-      <section className='footer__logo'>
+      <section className="footer__logo">
         <img src="/src/assets/restaurant-logo.png" alt="" />
         <h1>SwiftDeluxe Restaruant</h1>
       </section>
-      <ul className='footer__contacts'>
+      <ul className="footer__contacts">
         <li>SwiftDeluxeRestaurant@gmail.com</li>
         <li>Tel.: 073-251-739</li>
         <li>Sweden, Karlstad, Någonstans-Gata 6</li>
       </ul>
-      <section className='footer__social-media'>
-        <img className='footer__social-media-icon' src="/src/assets/facebook-logo.svg" alt="" />
-        <img className='footer__social-media-icon' src="/src/assets/instagram-logo.svg" alt="" />
+      <section className="footer__social-media">
+        <img
+          className="footer__social-media-icon"
+          src="/src/assets/facebook-logo.svg"
+          alt=""
+        />
+        <img
+          className="footer__social-media-icon"
+          src="/src/assets/instagram-logo.svg"
+          alt=""
+        />
       </section>
+      <Link to="/cart">
+        <button>Till Cartpage</button>
+      </Link>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
+
+//Författare Miklos
+// Ändring av Fredrick: La till en länk till cartpage för att kunna navigera runt sidan i samband med handledning. Tas bort senare.
