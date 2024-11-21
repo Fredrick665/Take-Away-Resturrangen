990c454499ecdb612d570424806b60b5169be433
Fredrick665 <fredrickaldrin@gmail.com>
2024-11-21 15:13:56 +0100

    Fixat menyn och så att man kan beställa!

---
+++ Backend/swift-deluxe-api/functions/userfunctions/getmenu.js
@@ -0,0 +1,30 @@
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocument, ScanCommand } = require("@aws-sdk/lib-dynamodb");
+
+const client = new DynamoDB();
+const db = DynamoDBDocument.from(client);
+
+const tableName = process.env.DYNAMODB_TABLE;
+
+exports.getMenuItems = async () => {
+  const params = {
+    TableName: tableName,
+  };
+
+  try {
+    const result = await db.send(new ScanCommand(params));
+
+    console.log("Successfully retrieved menu items:", result.Items);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+  } catch (error) {
+    console.error("Error occurred while retrieving menu items:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Could not retrieve menu items" }),
+    };
+  }
+};
---
+++ Backend/swift-deluxe-api/functions/userfunctions/neworder.js
@@ -0,0 +1,50 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { v4: uuidv4 } = require("uuid");
+
+const orderTableName = "Orders_SwiftDeluxe";
+
+const dynamoDBClient = new DynamoDBClient({ region: "eu-north-1" });
+
+exports.neworder = async (event) => {
+  try {
+    const requestBody = JSON.parse(event.body);
+    const { orderItems } = requestBody;
+
+    if (!orderItems || !Array.isArray(orderItems) || orderItems.length === 0) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "orderItems är obligatoriskt och måste vara en array",
+        }),
+      };
+    }
+
+    const orderId = uuidv4();
+
+    const newOrder = {
+      id: orderId,
+      orderItems: orderItems,
+      status: "PENDING",
+      createdAt: new Date().toISOString(),
+    };
+
+    const params = {
+      TableName: orderTableName,
+      Item: newOrder,
+    };
+
+    await dynamoDBClient.send(new PutCommand(params));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Order skapad!", orderId: orderId }),
+    };
+  } catch (error) {
+    console.error("Fel vid skapande av order:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Fel vid skapande av order" }),
+    };
+  }
+};
