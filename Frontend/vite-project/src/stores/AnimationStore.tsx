2e520d0b4a231315b271e1c63294a501efe0987d
Fredrick665 <fredrickaldrin@gmail.com>
2024-12-05 11:42:35 +0100

    La till Katarinas animationer i animation store

--- Frontend/vite-project/src/stores/AnimationStore.tsx
+++ Frontend/vite-project/src/stores/AnimationStore.tsx
@@ -231,4 +231,135 @@ const pulseRotateBall = {
 //    transition={pulseRotateBall.transition}
 // ></motion.figure>
 */
+
+// Katarinas Exempel
+/*
+ <motion.figure
+                    className="vector-left"
+                    onClick={decrementMinutes}
+                    whileTap={{
+                        scale: 1.6,
+                        rotate: -20
+                    }}
+                    transition={{ duration: 0.2 }}
+                > 
+                                <motion.div
+                className="set-btn"
+                initial={{ y: 0, scale: 1, backgroundColor: 'rgba(255, 255, 255, 0)' }}
+                animate={isClicked
+                    ? { y: 100, opacity: 0, scale: 0.3, backgroundColor: 'rgb(0, 255, 0)' }
+                    : { y: 0, opacity: 1, backgroundColor: 'rgba(255, 255, 255, 0)' }}
+
+                transition={{
+                    duration: 0.5
+                }}
+            > const handleClick = () => {
+        anime({
+            targets: 'img',
+            rotate: [90, -360],     // startposition 90deg, rotation360deg
+            duration: 1000,
+            easing: 'linear',
+            filter: 'invert(1) '
+        })
+        // anim. för att förstora icon och text
+        anime({
+            targets: '.loading',
+            scale: 1.2,
+            duration: 400,
+            easing: 'easeInOutQuad',
+            complete: () => {
+                navigate('/set-timer');
+            }
+        });
+    }
+    return (
+        <div className='loading' onClick={handleClick}>             <motion.section
+                className="digital-btn"
+                initial={{
+                    scale: 1,
+                    rotate: 0,
+                    fontSize: 14
+                }}
+                animate={isClicked
+                    ? {
+                        scale: 1.6,
+                        rotate: 360,
+                        fontSize: 20
+                    }
+                    : {
+                        scale: 1,
+                        rotate: 0
+                    }
+                }
+                transition={{ duration: 0.5 }}
+
+            >  anime({
+            targets: '.alarm-btn',
+            scale: [1, 2, 0.1],
+            duration: 900,
+            easing: 'easeInOutQuad',
+            opacity: [1, 1, 0],
+            complete: () => {
+                navigate('/set-timer');
+            }
+        });
+    }; <Btn text="SET NU TIMER" onClick={handleSetNuTimer} />  const menuItemVariants = {
+        hidden: {
+            opacity: 0,
+            x: 100
+        },    // initial transparens 0, flyttas till höger
+        visible: (i) => ({
+            opacity: 1,
+            x: 0,
+            transition: {
+                delay: i * 0.2 + 0.2,
+                duration: 0.4
+            },
+        }),
+    };
+
+    //initial transparens för icon
+    const iconVariants = {
+        hidden: { opacity: 0, x: 100, filter: 'invert(0.8)' },
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                duration: 0.4,
+                delay: 0.2,
+            },
+        },
+        clicked: {
+            x: 0,
+            rotate: 360,
+            filter: 'invert(0.5)',
+            scale: 0.8,
+            transition: {
+                duration: 0.9,
+                ease: 'linear',
+            },
+        },
+    };
+  motion.img
+                src={navicon}
+                alt="nav icon"
+                className="img"
+                variants={iconVariants}
+                initial="hidden"
+                animate={isIconClicked ? 'clicked' : 'visible'}    //function att stänga anim
+                onClick={handleIconClick}
+                style={{ filter: isIconClicked ? 'invert(1)' : 'invert(0)' }}
+            />
+            {['digital', 'analog', 'visual'].map((option, index) => (
+                <motion.h1
+                    key={option}
+                    variants={menuItemVariants}
+                    initial='hidden'
+                    animate='visible'   // användr animation
+                    custom={index}      //som argument till animation
+                    onClick={() => handleSelect(option)}
+                >
+                    {option.toUpperCase()}
+                </motion.h1>
+                */
 // Författare Fredrick
